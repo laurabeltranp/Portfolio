d6f9f175eddd3c99745ac55275ac77b09d9cc26e
Laura <laurabeltranp15@gmail.com>
2024-07-09 16:04:43 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import "@/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;500&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;500&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
